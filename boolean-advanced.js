228e957daed637f8193ca3dc8c70fb9e2b2e5343
Bryce <bryce.may.8@hotmail.com>
2019-06-26 15:33:53 -0600

    if else

--- boolean-advanced.js
+++ boolean-advanced.js
@@ -1,7 +1,23 @@
 let isAccountLocked = false
+let userRole = 'user'
 
 if (isAccountLocked) {
     console.log('is account locked')
+} else if (userRole === 'admin') {
+    console.log('welcome admin')
 } else {
-    console.log('welcome')
+    console.log('Welcome')
 }
+
+let temp = 100
+
+if (temp <= 32){
+    console.log('Its freezing outside')
+} else if (temp >= 100) {
+    console.log('its hot outside')
+} else {
+    console.log('its pretty nice')
+}
+// its freezing outside
+//its hot outside 
+//go for it, its pretty nice
---
+++ logical-andOr.js
@@ -0,0 +1,7 @@
+let temp = 95
+
+//Logical and operator
+
+if (temp >= 60 && temp <= 90) { 
+    console.log('its pretty nice out')
+}
